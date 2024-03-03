22c994d4203dee69db78f87faf26dd02abbb1af5
Comp46723 <maus789er@gmail.com>
2024-03-03 09:40:28 +0200

    maus

--- lp1.py
+++ lp1.py
@@ -1,1 +1,10 @@
-print("Привіт")
+class Human:
+    def __init__(self, name = "Human"):
+        self.name = name
+
+
+class Auto:
+    def __init__(self, brand):
+        self.brand = brand
+        self.passengers = []
+
